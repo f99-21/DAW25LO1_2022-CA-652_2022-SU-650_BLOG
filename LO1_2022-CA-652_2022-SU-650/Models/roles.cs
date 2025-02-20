2a0c34209f51c496c1a06a89739b9537f5ec834d
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-02-20 17:46:39 -0600

    creacion de clases

---
+++ LO1_2022-CA-652_2022-SU-650/Models/roles.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LO1_2022_CA_652_2022_SU_650.Models
+{
+    public class roles
+    {
+
+        [Key]
+
+        public int rolId {  get; set; }
+
+        public string rol {  get; set; }
+
+
+    }
+}
