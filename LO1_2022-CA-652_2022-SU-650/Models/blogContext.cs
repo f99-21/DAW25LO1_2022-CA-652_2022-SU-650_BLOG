fd34e4a5f54abae16320b654b1af21eb39ff825c
JoseChinchillaAlvarezzz <jose.chinchilla@catolica.edu.sv>
2025-02-20 17:33:28 -0600

    Creacion de carpeta Models e inyeccion del ConnectionString

---
+++ LO1_2022-CA-652_2022-SU-650/Models/blogContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.Extensions.Options;
+
+namespace LO1_2022_CA_652_2022_SU_650.Models
+{
+    public class blogContext : DbContext
+    {
+
+        public blogContext(DbContextOptions<blogContext> options) : base(options) 
+        {
+
+        }
+    }
+}
--- LO1_2022-CA-652_2022-SU-650/Program.cs
+++ LO1_2022-CA-652_2022-SU-650/Program.cs
@@ -1,6 +1,18 @@
+using LO1_2022_CA_652_2022_SU_650.Models;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+
+// Inyeccion por dependencia del string de conexion al contexto
+builder.Services.AddDbContext<blogContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("blogDbConnection")
+                ));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
